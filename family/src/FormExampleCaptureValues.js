e5a4a0ba417bbfee3ccee50e6d0f365d129dcd35
Neota Moe <neota.moe@gmail.com>
2017-10-04 22:16:49 -0500

    added favorite number field

--- family/src/FormExampleCaptureValues.js
+++ family/src/FormExampleCaptureValues.js
@@ -3,20 +3,20 @@ import {Form} from 'semantic-ui-react';
 
 class FormExampleCaptureValues extends Component {
 
-  state = { name: '', email: '', submittedName: '', submittedEmail: '', favoriteNumber: '' }
+  state = { name: '', email: '', submittedName: '', submittedEmail: '', favoriteNumber: '', submittedFavoriteNumber: '' }
 
-  handleChange = (e, { name, value }) => this.setState({ [name]: value })
+  handleChange = (e, { name, value, favoriteNumber }) => this.setState({ [name]: value })
 
   handleSubmit = () => {
     const { name, email, favoriteNumber } = this.state
 
-    this.setState({ submittedName: name, submittedEmail: email, favoriteNumber: favoriteNumber })
+    this.setState({ submittedName: name, submittedEmail: email, submittedFavoriteNumber: favoriteNumber })
 
     console.log('submittedName: ' + name + ' submittedEmail: ' + email );
   }
 
   render() {
-    const { name, email, submittedName, submittedEmail } = this.state
+    const { name, email, submittedName, submittedEmail, favoriteNumber, submittedFavoriteNumber } = this.state
 
     return (
       <div>
@@ -24,13 +24,14 @@ class FormExampleCaptureValues extends Component {
           <Form.Group>
             <Form.Input placeholder='Name' name='name' value={name} onChange={this.handleChange} />
             <Form.Input placeholder='Email' name='email' value={email} onChange={this.handleChange} />
+            <Form.Input placeholder='Favorite Number' name='favoriteNumber' value={favoriteNumber} onChange={this.handleChange} />
             <Form.Button content='Submit' />
           </Form.Group>
         </Form>
         <strong>onChange:</strong>
-        <pre>{JSON.stringify({ name, email }, null, 2)}</pre>
+        <pre>{JSON.stringify({ name, email, favoriteNumber}, null, 2)}</pre>
         <strong>onSubmit:</strong>
-        <pre>{JSON.stringify({ submittedName, submittedEmail }, null, 2)}</pre>
+        <pre>{JSON.stringify({ submittedName, submittedEmail, submittedFavoriteNumber }, null, 2)}</pre>
       </div>
     )
   }
