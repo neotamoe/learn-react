e0569bddf5359abc11a596d766efff955d3eeeb1
Neota Moe <neota.moe@gmail.com>
2017-10-05 21:36:13 -0500

    added dropdown field, but not grabbing value yet in onSubmit

--- family/src/FormExampleCaptureValues.js
+++ family/src/FormExampleCaptureValues.js
@@ -1,22 +1,28 @@
 import React, {Component} from 'react';
 import {Form} from 'semantic-ui-react';
 
+const options = [
+  { key: 'm', name: 'Male', text: 'Male', value: 'male' },
+  { key: 'f', name: 'Female', text: 'Female', value: 'female' },
+]
+
+
 class FormExampleCaptureValues extends Component {
 
-  state = { name: '', email: '', submittedName: '', submittedEmail: '', favoriteNumber: '', submittedFavoriteNumber: '' }
+  state = { name: '', email: '', submittedName: '', submittedEmail: '', favoriteNumber: '', submittedFavoriteNumber: '', submittedSex: '', sex: '' }
 
-  handleChange = (e, { name, value, favoriteNumber }) => this.setState({ [name]: value })
+  handleChange = (e, { name, value }) => this.setState({ [name]: value })
 
   handleSubmit = () => {
-    const { name, email, favoriteNumber } = this.state
+    const { name, email, favoriteNumber, sex } = this.state
 
-    this.setState({ submittedName: name, submittedEmail: email, submittedFavoriteNumber: favoriteNumber })
+    this.setState({ submittedName: name, submittedEmail: email, submittedFavoriteNumber: favoriteNumber, submittedSex: sex })
 
-    console.log('submittedName: ' + name + ' submittedEmail: ' + email );
+    console.log('submittedName: ' + name + ' submittedEmail: ' + email + ' submittedFavoriteNumber: ' + favoriteNumber + ' submittedSex: ' + sex);
   }
 
   render() {
-    const { name, email, submittedName, submittedEmail, favoriteNumber, submittedFavoriteNumber } = this.state
+    const { name, email, submittedName, submittedEmail, favoriteNumber, submittedFavoriteNumber, submittedSex, sex } = this.state
 
     return (
       <div>
@@ -25,13 +31,14 @@ class FormExampleCaptureValues extends Component {
             <Form.Input placeholder='Name' name='name' value={name} onChange={this.handleChange} />
             <Form.Input placeholder='Email' name='email' value={email} onChange={this.handleChange} />
             <Form.Input placeholder='Favorite Number' name='favoriteNumber' value={favoriteNumber} onChange={this.handleChange} />
+            <Form.Select options={options} placeholder='Sex' onChange={this.handleChange} />
             <Form.Button content='Submit' />
           </Form.Group>
         </Form>
         <strong>onChange:</strong>
-        <pre>{JSON.stringify({ name, email, favoriteNumber}, null, 2)}</pre>
+        <pre>{JSON.stringify({ name, email, favoriteNumber, sex}, null, 2)}</pre>
         <strong>onSubmit:</strong>
-        <pre>{JSON.stringify({ submittedName, submittedEmail, submittedFavoriteNumber }, null, 2)}</pre>
+        <pre>{JSON.stringify({ submittedName, submittedEmail, submittedFavoriteNumber, submittedSex }, null, 2)}</pre>
       </div>
     )
   }
