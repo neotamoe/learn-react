1555d429e43f189759ee4cca245b67af3613aa23
Neota Moe <neota.moe@gmail.com>
2019-01-11 20:23:21 -0600

    current weather api call and component

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -5,27 +5,38 @@ import Input from './components/input';
 import Button from './components/button';
 import axios from 'axios';
 import key from './key';
+import Current from './components/current';
 
 class App extends Component {
   state = {
     city: null,
     showDays: false,
-    days: []
+    days: [],
+    showCurrent: false,
+    current: []
   }
  
   handleSubmit = (e) => {
     e.preventDefault();
     console.log('The button was clicked.');
     console.log('current city: '+ this.state.city);
-    axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${this.state.city},us&units=imperial&mode=json&appid=${key}`).then((response) => {
-      this.setState((prevState, props) => {
-        return {
-          showDays: !prevState.showDays,
-          days: response.data.list
-        }
-      });
-      console.log(response.data.list);
-    })
+    axios.all([
+      axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${this.state.city},us&units=imperial&mode=json&appid=${key}` ),
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.city},us&units=imperial&mode=json&appid=${key}`)
+    ])
+    // .then(axios.spread(function (forecast, current) {
+    .then(response => this.setState((prevState, props) => {
+      console.log('response0: forecast', response[0].data);
+      console.log('response1: weather', response[1].data);
+      console.log('response[1].data.main', response[1].data.main.temp)
+      return {
+        days: response[0].data.list,
+        showDays: !prevState.showDays,
+        showCurrent: !prevState.showCurrent,
+        current: response[1].data
+      }
+    }))
+    .catch(error => console.log(error));
   }
 
   handleInputChange = (e) => {
@@ -36,7 +47,7 @@ class App extends Component {
 
   render() {
     const forecastDays = this.state.days.map((day) =>
-      <Day key={day.id} temperature={day.main.temp} description={day.weather[0].description} iconCode={day.weather[0].icon}></Day> 
+      <Day key={day.dt_txt} dateTime={day.dt_txt} temperature={day.main.temp} description={day.weather[0].main} iconCode={day.weather[0].icon}></Day> 
     );
     return (
       <div className="App">
@@ -45,6 +56,14 @@ class App extends Component {
             <Input labelName="City" value="" placeholder="Enter a City" handleInputChange={this.handleInputChange}/> 
             <Button type="submit" handleSubmit={this.handleSubmit} buttonText="Get Weather"></Button>
           </form>
+          <hr style={{margin: '30px'}} />
+          {this.state.showCurrent ? 
+            <Current 
+              city={this.state.current.name} 
+              temperature={this.state.current.main.temp.toFixed()} 
+              iconCode={this.state.current.weather[0].icon}
+              description={this.state.current.weather[0].main}/> 
+            : null }
           {this.state.showDays ? 
             <div className="days">
               {forecastDays}
--- weather-app/src/components/current.js
+++ weather-app/src/components/current.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const day = (props) => {
+const current = (props) => {
     return (
-        <div className="day">
-            <p>{props.dateTime}</p>
+        <div className="current">
+            <h1>Current Weather : {props.city}</h1>
             <img src={`http://openweathermap.org/img/w/${props.iconCode}.png`} alt="icon"></img>
-            <h2>{props.temperature.toFixed()}&#176;F</h2>
+            <h2>{props.temperature}&#176;F</h2>
             <p>{props.description}</p>
         </div>
     );
 };
 
-export default day;
+export default current;
