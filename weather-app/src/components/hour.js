43aa4d196efea7f2b0669e7e181b746382bd84aa
Neota Moe <neota.moe@gmail.com>
2019-01-24 18:32:40 -0600

    minor style tweaks

--- weather-app/src/components/hour.js
+++ weather-app/src/components/hour.js
@@ -4,6 +4,7 @@ const hour = (props) => {
     return (
         <div className={`${props.className} day`}>
             <p>{props.time}</p>
+            <hr />
             <img src={`http://openweathermap.org/img/w/${props.iconCode}.png`} alt="icon"></img>
             <p><strong>{props.temperature.toFixed()}&#176;F</strong></p>
             <p style={{fontSize: '10px'}}>{props.description}</p>
